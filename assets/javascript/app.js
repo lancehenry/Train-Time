886f95aeba20df7e4437ad60676fa1580b3ab98b
Lance Schmittling <lanceschmittling@lances-imac.ncct.com>
2018-03-01 16:17:35 -0600

    Added some JS functionality

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -16,13 +16,75 @@ var database = firebase.database();
 // Variables to store data
 var name;
 var destination;
-var frequency;
+var trainTime;
+var tFrequency;
 var nextArrival;
 var minutesAway;
 
-$("#submitBtn").on("click", function () {
+$("#submitBtn").on("click", function (event) {
+    event.preventDefault();
+
+    // Store the most recent train information entered
+    name = $("#trainName").val().trim();
+    destination = $("#trainDestination").val().trim();
+    trainTime = $("#trainTime").val().trim();
+    tFrequency = $("#trainFrequency").val().trim();
+
+    // Calculate Next Arrival time
 
 
 
+
+
+    // Calculate Minutes Away time
     
-})
+
+
+
+
+    // Log what you're storing from above
+    console.log(name);
+    console.log(destination);
+    console.log(trainTime);
+    console.log(tFrequency);
+
+    // Code for the push to firebase
+    database.ref().push({
+        name: name,
+        destination: destination,
+        time: trainTime,
+        frequency: tFrequency,
+        dateAdded: firebase.database.ServerValue.TIMESTAMP
+    });
+});
+
+// Firebase watcher + initial loader
+database.ref().on("child_added", function (childSnapshot) {
+
+    console.log(childSnapshot.val().name);
+    console.log(childSnapshot.val().destination);
+    console.log(childSnapshot.val().time);
+    console.log(childSnapshot.val().frequency);
+
+// Handles the errors
+}, function (errorObject) {
+    console.log("Errors handled: " + errorObject.code);
+});
+
+database.ref().orderByChild("dateAdded").limitToLast(1).on("child_added", function (snapshot) {
+
+    // Change the HTML to reflect
+    var sv = snapshot.val();
+
+    var html =
+        '<tr class="test">' +
+        '<td>' + sv.name + '</td>' +
+        '<td>' + sv.destination + '</td>' +
+        '<td>' + sv.frequency + '</td>' +
+        // '<td>$ ' + sv.nextArrival + '</td>' +
+        // '<td>$ ' + sv.minutesAway + '</td>' +
+        '</tr>';
+
+    $('#outPutRow').append(html);
+
+});
